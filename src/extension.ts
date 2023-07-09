d4fefe7feb01df0169e2e1fc76945b22e23ce86e
Nathan Ward <175123+vine77@users.noreply.github.com>
2023-07-08 21:01:31 -0700

    Rename project to ignite-code-extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
+import * as vscode from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -8,39 +8,39 @@ export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log(
-    'Congratulations, your extension "code-extension-demo" is now active!'
+    'Congratulations, your extension "ignite-code-extension" is now active!',
   );
 
   let subscriptions = [
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    vscode.commands.registerCommand("code-extension-demo.helloWorld", () => {
+    vscode.commands.registerCommand('ignite-code-extension.helloWorld', () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
       vscode.window.showInformationMessage(
-        "Hello World from code-extension-demo!"
+        'Hello World from ignite-code-extension!',
       );
     }),
 
     vscode.window.onDidChangeActiveTextEditor((e) => {
-      console.log("onDidChangeActiveTextEditor", e);
+      console.log('onDidChangeActiveTextEditor', e);
     }),
 
     vscode.window.onDidChangeTextEditorSelection((e) => {
-      console.log("onDidChangeTextEditorSelection", e);
+      console.log('onDidChangeTextEditorSelection', e);
     }),
 
     vscode.window.onDidChangeActiveNotebookEditor((e) => {
-      console.log("onDidChangeActiveNotebookEditor", e);
+      console.log('onDidChangeActiveNotebookEditor', e);
     }),
 
     vscode.window.onDidChangeNotebookEditorSelection((e) => {
-      console.log("onDidChangeNotebookEditorSelection", e);
+      console.log('onDidChangeNotebookEditorSelection', e);
     }),
 
     vscode.window.onDidChangeWindowState((e) => {
-      console.log("onDidChangeWindowState", e);
+      console.log('onDidChangeWindowState', e);
     }),
   ];
 
