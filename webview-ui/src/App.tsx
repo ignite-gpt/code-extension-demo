e61ed093c0983c972a22d8627effffc53d07d028
Nathan Ward <175123+vine77@users.noreply.github.com>
2023-07-09 23:13:16 -0700

    Add React input field

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -1,18 +1,27 @@
+import { useState } from 'react'
 import { vscode } from './utilities/vscode'
-import { VSCodeButton } from '@vscode/webview-ui-toolkit/react'
+import { VSCodeButton, VSCodeTextField } from '@vscode/webview-ui-toolkit/react'
 import './App.css'
 
 function App() {
+  const [inputValue, setInputValue] = useState('')
+
   function handleHowdyClick() {
     vscode.postMessage({
       command: 'hello',
-      text: 'Hey there partner! 🤠',
+      text: inputValue,
     })
   }
 
   return (
     <main>
       <h1>Hello World!</h1>
+      <VSCodeTextField
+        placeholder="Type something here"
+        value={inputValue}
+        onChange={(e) => setInputValue((e.target as HTMLInputElement).value)}
+        style={{ marginBottom: '8px' }}
+      />
       <VSCodeButton onClick={handleHowdyClick}>Howdy!</VSCodeButton>
     </main>
   )
